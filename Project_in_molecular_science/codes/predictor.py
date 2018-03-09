ed42d3c39532636a116bd8ad43c437be4e5cec5f
Sharmishtaa Kumar <sharmishtaakumar@gmail.com>
2018-03-09 17:11:50 +0100

    new

---
+++ Project_in_molecular_science/codes/predictor.py
@@ -0,0 +1,42 @@
+import newsvm
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.externals import joblib
+tempfile = "../datasets/test1"
+
+savedmodel= joblib.load('firstmodel.sav')
+testX=newsvm.predictor(tempfile)
+    
+
+predicted=savedmodel.predict(testX)
+#print(predicted)
+
+topology_dict= {2:'I',4:'M',6:'O'}
+predicted_list=predicted.tolist()
+#print(predicted_list)
+list_tops=[]
+for number in predicted_list:
+    #print(number)
+	list_tops.extend(topology_dict[number])
+new_tops=list_tops
+#print(list_tops)   
+
+newout=0
+backto=0
+newlist=[]
+filehandle=open(tempfile,'r')
+text=filehandle.read().splitlines()
+with open("predicted.txt",'w') as pr:
+    for h in range(len(text)):
+	    if text[h].startswith('>'):
+		    pr.write(text[h])
+		    pr.write("\n")
+		    pr.write(text[h+1])
+		    pr.write("\n")
+		    newout=newout+len(text[h+1])
+		    #print(newout)
+		    l=''.join(list_tops[backto:newout])
+		    pr.write(l)
+		    pr.write("\n")
+		    backto=newout
