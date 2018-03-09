a39a95b94263e104c9b243ffa4642701844ee863
Sharmishtaa Kumar <sharmishtaakumar@gmail.com>
2018-03-09 16:24:41 +0100

    changes

---
+++ Project_in_molecular_science/general_scripts/bashscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+mkdir Project_in_molecular_science
+cd Project_in_molecular_science
+ mkdir datasets
+ ls datasets
+ mkdir general_scripts
+ ls general_scripts
+ mkdir bin
+ ls bin
+ mkdir codes
+ ls codes
+ 
+
