e350ac7eacce32c1f4c3abf4010473a459eea75e
Sharmishtaa Kumar <sharmishtaakumar@gmail.com>
2018-03-23 15:41:29 +0100

    new

--- Project_in_molecular_science/codes/All_models.py
+++ Project_in_molecular_science/codes/All_models.py
@@ -5,15 +5,16 @@ from sklearn.model_selection import cross_val_score
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier as rfc
 from sklearn.externals import joblib
-test = "../datasets/test"
+tempfile = "../datasets/test"
 def run_svm (filename) :   
-    trainX,trainY=modelcv.dataset(test,31)
+    trainX,trainY=modelcv.modelinput(tempfile,31)
+    print (trainX.shape, trainY.shape)
     clf = svm.SVC(kernel='linear',C=1, gamma=0.01)
     clf.fit(trainX, trainY)
     inputfile='../models/firstmodel_SVC.sav'
     joblib.dump(clf,inputfile)
 def decisiontree(filename):
-    trainX,trainY=modelcv.dataset(test,33)
+    trainX,trainY=modelcv.modelinput(tempfile,33)
     clf = tree.DecisionTreeClassifier(min_samples_split = 5,class_weight='balanced')
     clf.fit(trainX, trainY)
     #score=cross_val_score(clf, trainX, trainY, cv=3,verbose=True,n_jobs=-1)
@@ -21,7 +22,7 @@ def decisiontree(filename):
     inputfile='../models/DTmodel.sav'
     joblib.dump(clf,inputfile)
 def randomforest(filename):
-    trainX,trainY=modelcv.dataset(test,33)
+    trainX,trainY=modelcv.modelinput(tempfile,33)
     clf = rfc(n_estimators=250, min_samples_split =6, n_jobs=-1, class_weight='balanced')
     clf.fit(trainX, trainY)
     #score=cross_val_score(clf, trainX, trainY, cv=3,verbose=True,n_jobs=-1)
@@ -30,6 +31,6 @@ def randomforest(filename):
     joblib.dump(clf,inputfile,compress=9)
           
 if __name__=="__main__":
-    run_svm('test')
-    decisiontree('test')
-    randomforest('test')
+    run_svm(tempfile)
+    decisiontree(tempfile)
+    randomforest(tempfile)
--- Project_in_molecular_science/codes/modelcv.py
+++ Project_in_molecular_science/codes/modelcv.py
@@ -2,28 +2,41 @@ import numpy as np
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
 from sklearn.externals import joblib
-test="../datasets/test"
-def dataset(filename,winlen):
-    filehandle = open(filename,'r')
-    text = filehandle.readlines()
+test="../datasets/test2"
+def lists(filename):
     identity=[]
     sequences=[]
     topologies=[]
-    y=[]
-    dictionary={}
-    A='0'
-##########identity#########
-    for line in text:
-        if line[0]=='>':
+    for i, line in enumerate(open(filename,"r")):
+        if i%3==0:
             identity.append(line.rstrip())
-    #print(identity)
+        elif i%3==1:
+            sequences.append(line.rstrip())
+        else:
+            topologies.append(line.rstrip())
+            
+    return (identity,sequences,topologies)
+def listofchar(filename):
+    sequences=lists(filename)[1]
+    listchars=[]
+    for seqs in sequences:
+        temp=[]
+        for char in seqs:
+            temp.append(char)
+        listchars.append(temp)
     
-##########topology##########
-    for line in text:
-        if line[0]!= '>': 
-            if line[0]!= 'M':
-            	topologies.append(line.rstrip())
-    #print (topologies)    	
+    return(listchars)
+def padding(filename,winlen):
+    sequences=listofchar(filename)
+    pad=(int(winlen)//2)*['0']
+    #print(pad)
+    finalseq=[]
+    for seqs in sequences:
+        #print(seqs)
+        finalseq.append(pad+seqs+pad)
+    return(finalseq)
+def topology(filename):
+    topologies = lists(filename)[2]         
     dicttop = {'I': 2, 'M': 4, 'O': 6}
     #print(list(dicttop.items()))
     new = [] 
@@ -33,72 +46,72 @@ def dataset(filename,winlen):
         for z in topo:
             list_A.append(dicttop[z])
         #print(list_A)
-        y.extend(list_A)   
+        y.append(list_A)   
     #print(y)
     #print(len(y))
     P=np.array(y)
-    #print(P)
- 
-##########aminoacids#######
-    for line in text:
-        if line[0]=='M':
-        	sequences.append(line.rstrip())
-        	for seq in sequences:
-        	    #print (sequences)
-        	    seqs=''.join(sequences)
-        	    #print(seqs)
-        	    pad=int(winlen)//2
-        	    newseq=list(seqs)
-        	    #print(newseq)
-        	    sequence=(pad*[A])+(newseq)+(pad*[A])
-        	    #print(sequence)
-        	    #print(len(sequence))
-    #print(len(newseq))
-        	
+    return (P)
+def dataset(filename,winlen):
+    sequence = padding(filename,winlen)
     dictseq={}    
     a_a=['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
-    #print (a_a)
     binary= np.zeros(shape=(20,20), dtype=int)
     np.fill_diagonal(binary, 1)
-    #print (binary)
     newbinary=binary.tolist()
     dictionary= zip(a_a,newbinary)
     dictseq = dict(dictionary)
     c={'0': [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
     dictseq.update(c)
-    #print (c)
-    #print (dictseq)
+    finallist=[]
+    for seqs in sequence:
+        temp=[]
+        for char in seqs:
+            temp2=dictseq[char]
+            temp.append(temp2)
+        finallist.append(temp)
+    finallist = np.array(finallist)
     
-    
-    i=iter(sequence)
-    numofwin = int(len(sequence)-int(winlen)/1)+1
+    return(finallist)
+def windows(filename,winlen):
+    sequence = dataset(filename,winlen)
     hey=[]
-    for i in range(0,numofwin):
-    		newseq=(sequence[i:i+int(winlen)])
-    		#print(newseq)
-    		arr=''.join(newseq)
-    		#print(arr)
-    		hey.append(arr)
-    #print(hey)
-    listnew=[]
-    seqlist=[]	
-    for j in hey:
-        #print(j)
-        newlist=[]
-        for k in j:
-            for key,value in dictseq.items():
-                #print(value)
-                if k==key:
-                    newlist.extend(value)
-                    #print(newlist)
-        seqlist.append(newlist)
-    #print(seqlist)
-    listnew=seqlist
-    #print(len(listnew))
-    X=np.array(listnew)
-    #print(len(X))
-    return X, P
+    n = (winlen-1)/2
+    n = int(n)
+    for seqs in sequence:
+        temp=[]
+        for i in range(0,len(seqs) - winlen + 1):
+            newwindow=np.array(seqs[i:i+int(winlen)])
+            a = newwindow.flatten()
+            temp.append(a)
+        hey.append(temp)
+    hey = np.array(hey)
+    #print(hey.shape)
+    return hey
+def modelinput(filename,winlen):
+    seq= windows(filename,winlen)
+    top=topology(filename)
+    listofwindows=[]
+    listoftop=[]
+    for seqs in seq:
+        listofwindows.extend(seqs)
+    for topo in top:
+        listoftop.extend(topo)
+    X=np.array(listofwindows)
+    Y=np.array(listoftop)
+    return X,Y
+def predictorinput(filename,winlen):
+    identity=lists(filename)[0]
+    sequence=lists(filename)[1]
+    seqs=windows(filename,winlen)
+    return identity, sequence, seqs
     
+
+if __name__=="__main__":
+     
+    #print(dataset(test, 31))
+    #windows(test, 31)
+    modelinput(test, 31)
+             
   
         
         
